cfa6d90178c73a66fbe65c00917524ab9e5b1d1d
Ibrahimdk <ibrahimmahardhikaid@gmail.com>
2024-05-31 15:05:15 +0700

    update home student

--- src/app/components/ButtomNavigation.tsx
+++ src/app/components/ButtomNavigation.tsx
@@ -2,24 +2,23 @@ import Link from "next/link";
 
 const ButtomNavigation = () => {
   return (
-    <div className="fixed  w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-0 left-1/2 dark:bg-gray-700 dark:border-gray-600">
+    <div className="shadow shadow-blue-600 fixed bottom-0 left-0 right-0 gap-10 flex justify-center mx-auto px-6 mt-11 w-full sm:w-auto md:w-1/2 lg:w-1/3 xl:w-1/4 bg-white rounded-t-[40px] pt-2 pb-1 ">
       <div className="flex h-full max-w-lg items-center justify-center gap-14">
         <button data-tooltip-target="tooltip-home" type="button" className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
           <Link href="/dashboard/student">
-            <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-              <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-            </svg>
-            <span className="sr-only">Home</span>
+          <svg className="hover:scale-150 duration-150  " width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M2 12.2039C2 9.91549 2 8.77128 2.5192 7.82274C3.0384 6.87421 3.98695 6.28551 5.88403 5.10813L7.88403 3.86687C9.88939 2.62229 10.8921 2 12 2C13.1079 2 14.1106 2.62229 16.116 3.86687L18.116 5.10812C20.0131 6.28551 20.9616 6.87421 21.4808 7.82274C22 8.77128 22 9.91549 22 12.2039V13.725C22 17.6258 22 19.5763 20.8284 20.7881C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.7881C2 19.5763 2 17.6258 2 13.725V12.2039Z" stroke="#2e3e76" stroke-width="1.5"></path> <path d="M15 18H9" stroke="#2e3e76" stroke-width="1.5" stroke-linecap="round"></path> </g></svg> 
+          <p className="text-[12px]  font-medium text-blue-900">Home</p>
+            {/* <span className="sr-only">Home</span> */}
           </Link>
         </button>
 
         <button
           data-tooltip-target="tooltip-new"
           type="button"
-          className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
+          className="inline-flex items-center justify-center w-12 h-12 font-medium bg-blue-600 rounded-2xl hover:bg-blue-400 duration-200 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
         >
           <Link href="/dashboard/student/scan">
-            <svg className="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <svg className="w-6 h-6 ml-1 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
               <path
                 stroke="currentColor"
                 stroke-linecap="round"
@@ -28,22 +27,16 @@ const ButtomNavigation = () => {
                 d="M21 12a28.076 28.076 0 0 1-1.091 9M7.231 4.37a8.994 8.994 0 0 1 12.88 3.73M2.958 15S3 14.577 3 12a8.949 8.949 0 0 1 1.735-5.307m12.84 3.088A5.98 5.98 0 0 1 18 12a30 30 0 0 1-.464 6.232M6 12a6 6 0 0 1 9.352-4.974M4 21a5.964 5.964 0 0 1 1.01-3.328 5.15 5.15 0 0 0 .786-1.926m8.66 2.486a13.96 13.96 0 0 1-.962 2.683M7.5 19.336C9 17.092 9 14.845 9 12a3 3 0 1 1 6 0c0 .749 0 1.521-.031 2.311M12 12c0 3 0 6-2 9"
               />
             </svg>
-            <span className="sr-only">Scan</span>
+            <p className="text-[12px]  font-medium text-white">Scan</p>
+            {/* <span className="sr-only">Scan</span> */}
           </Link>
         </button>
 
         <button data-tooltip-target="tooltip-settings" type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
           <Link href="/dashboard/student/riwayat">
-            <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"
-              />
-            </svg>
-            <span className="sr-only">Settings</span>
+          <svg className="ml-1 hover:scale-150 duration-150 " width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9.5 14.4L10.9286 16L14.5 12" stroke="#2e3e76" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M2 11.9997C2 7.28562 2 4.9286 3.46447 3.46413C4.70529 2.22331 6.58687 2.03382 10 2.00488M22 11.9997C22 7.28562 22 4.9286 20.5355 3.46413C19.2947 2.22331 17.4131 2.03382 14 2.00488" stroke="#2e3e76" stroke-width="1.5" stroke-linecap="round"></path> <path d="M10 22C7.19974 22 5.79961 22 4.73005 21.455C3.78924 20.9757 3.02433 20.2108 2.54497 19.27C2 18.2004 2 16.8003 2 14C2 11.1997 2 9.79961 2.54497 8.73005C3.02433 7.78924 3.78924 7.02433 4.73005 6.54497C5.79961 6 7.19974 6 10 6H14C16.8003 6 18.2004 6 19.27 6.54497C20.2108 7.02433 20.9757 7.78924 21.455 8.73005C22 9.79961 22 11.1997 22 14C22 16.8003 22 18.2004 21.455 19.27C20.9757 20.2108 20.2108 20.9757 19.27 21.455C18.2004 22 16.8003 22 14 22" stroke="#2e3e76" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
+          <p className="text-[12px]  font-medium text-blue-900">History</p>
+            {/* <span className="sr-only">Settings</span> */}
           </Link>
         </button>
       </div>
--- src/app/components/CardRoom.tsx
+++ src/app/components/CardRoom.tsx
@@ -14,24 +14,24 @@ interface CardRoomProps {
 
 const CardRoom: React.FC<CardRoomProps> = ({ className, classPlace, classDay, classTime, group_code, sks, LectureName, LectureNip, LecturePhone }) => {
   return (
-    <div className="w-full h-40 p-3 rounded-lg bg-white text-white flex flex-col gap-3 font-semibold shadow-md">
+    <div className="w-full h-40 rounded-3xl hover:bg-blue-900  duration-300 group bg-white text-white flex flex-col shadow-md ">
       {/* hari */}
-      <div className="box bg-yellow-300 rounded-full w-1/2">
-        <p className="text-center text-sm">{classDay}</p>
+      <div className="box  text-blue-900  rounded-full w-1/2  ">
+        <p className="text-white px-4 group-hover:bg-white duration-300 group-hover:text-blue-900 font-bold text-sm bg-blue-900 rounded-tl-[20px] rounded-br-2xl   py-2 ml-0 mt-0">{classDay}</p>
       </div>
       {/* nama kelas */}
-      <div className="box bg-blue-700 rounded-full w-50 line-clamp-1 text-base  text-ellipsis">
-        <p className="pl-3 text-sm">{className}</p>
+      <div className="box mt-4 w-50 line-clamp-1 text-base px-3 text-ellipsis uppercase">
+        <p className="text-blue-900  font-medium group-hover:text-white">{className}</p>
       </div>
 
       <div className="box text-sm flex gap-1 line-clamp-1 text-ellipsis">
-        <p className="waktu px-3 bg-slate-500 rounded-full text-sm">{classTime}</p>
-        <p className="tempat px-3 bg-slate-400 rounded-full text-sm">{classPlace}</p>
+        <p className="waktu pl-3 text-blue-900 group-hover:text-white text-sm">{classTime} - </p>
+        <p className="waktu text-blue-900 group-hover:text-white text-sm">{classPlace}</p>
       </div>
 
-      <div className="box text-sm flex gap-2">
-        <p className="waktu px-3 bg-slate-500 rounded-full text-sm">
-          {group_code} - {sks} sks
+      <div className="box text-sm flex px-3 gap-2">
+        <p className="text-blue-900  text-sm group-hover:text-white">
+          {group_code} - {sks} SKS
         </p>
       </div>
     </div>
